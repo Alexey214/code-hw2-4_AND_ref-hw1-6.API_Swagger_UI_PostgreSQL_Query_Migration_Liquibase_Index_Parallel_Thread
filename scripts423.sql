61d5719a4eed012287402f8a9ce90abedb6af16d
brain <golenev214@gmail.com>
2022-08-04 21:21:58 +0300

    ref-hw2 - result + fixing problems

--- scripts423.sql
+++ scripts423.sql
@@ -1,6 +1,6 @@
 select s.name, s.age, f.name
 from student s
-         inner join faculty f on faculty_id = f.id;
+         left join faculty f on faculty_id = f.id;
 
 select s.id, s.name, s.age
 from student s
